ea0a50484c8cc0aa99ebc7135aa3da670f32af20
Estelle DeBlois <edeblois@gmail.com>
2015-03-22 00:52:50 -0400

    Support for packaging
    * Upgrade to ember-cli-node-webkit 0.2.0
    * Prevent node files from being fingerprinted during build
    * Custom packaging config

---
+++ config/nw-package.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Export a config object for node-webkit-builder.
+// See https://github.com/mllrsohn/node-webkit-builder#options.
+
+module.exports = {
+  appName: 'Markdown Editor'
+};
