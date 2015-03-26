bffc32439e4d8885e0124486c6c3f08ff25a0151
Estelle DeBlois <edeblois@gmail.com>
2015-03-26 13:38:57 -0400

    Add app icons and up version number

--- config/nw-package.js
+++ config/nw-package.js
@@ -4,5 +4,11 @@
 // See https://github.com/mllrsohn/node-webkit-builder#options.
 
 module.exports = {
-  appName: 'Markdown Editor'
+  appName: 'Markdown Editor'//,
+  // Enable these options to include an application icon.
+  // On a Mac, you must have Wine installed in order to package the app
+  // for Windows with icon.
+  //platforms: ['osx64', 'win64'],
+  //winIco: 'build/icons/emberjs-logo.ico',
+  //macIcns: 'build/icons/emberjs-logo.icns'
 };
