0f871042ae481545da08ef8825aafa704f773694
Estelle DeBlois <edeblois@gmail.com>
2015-05-24 01:31:36 -0400

    Packaging for all 3 operating systems: win, oxs, linux

--- config/nw-package.js
+++ config/nw-package.js
@@ -4,11 +4,12 @@
 // See https://github.com/mllrsohn/node-webkit-builder#options.
 
 module.exports = {
-  appName: 'Markdown Editor'//,
+  appName: 'Markdown Editor',
   // Enable these options to include an application icon.
   // On a Mac, you must have Wine installed in order to package the app
   // for Windows with icon.
-  //platforms: ['osx64', 'win64'],
-  //winIco: 'build/icons/emberjs-logo.ico',
-  //macIcns: 'build/icons/emberjs-logo.icns'
+  platforms: ['osx64', 'win64', 'linux64'],
+  winIco: 'build/icons/emberjs-logo.ico',
+  macIcns: 'build/icons/emberjs-logo.icns',
+  version: '0.12.2'
 };
